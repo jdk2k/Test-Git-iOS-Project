c7377357ae0034f4c3e85f3ec5a8975a809f7053
Jason Kimme <jdk82@me.com>
2014-09-04 12:43:26 -0700

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by jk on 9/4/14.
+//  Copyright (c) 2014 jk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by jk on 9/4/14.
+//  Copyright (c) 2014 jk. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
