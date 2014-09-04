ff270728c3dda28ccdeb3c8dea2dbfad5b1594d4
jk <Jason@Jasons-MacBook-Pro.local>
2014-09-04 11:42:49 -0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by jk on 9/4/14.
+//  Copyright (c) 2014 jk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by jk on 9/4/14.
+//  Copyright (c) 2014 jk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
